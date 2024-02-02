f0f35cc9db7da740d6f6035bda82e10429b5095e
mphonethathe3 <148745003+mphonethathe3@users.noreply.github.com>
2024-02-02 08:26:15 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter 1st number:");
+        int num1 = in.nextInt();
+        System.out.println("Enter 2nd number:");
+        int num2 = in.nextInt();
+
+//declare variables
+        double sum = num1 + num2;
+          if (sum=65 || num1,num2 == 65){
+              System.out.println("Sum" + false);
+        }
+          else if(){
+
+        }
+
+          }
+        public static void longest ("the", "quick", "brown", "fox", "ate", "my", "chickens"){
+            longest(words);
+
+            if (strings == null || strings.length == 0) {
+                System.out.println("The list is empty.");
+                return;
+            }
+
+            String longestStr = strings[0];
+            for (String string : strings) {
+                if (string.length() > longestStr.length()) {
+                    longestStr = string;
+                }
+            }
+
+            System.out.println(longestStr);
+        }
+    }
+
+
+
+
+
+
+
+
